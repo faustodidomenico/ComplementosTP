10e1705bfc962b45a0247920428deaec580d574a
faustodidomenico <faustodido@gmail.com>
2018-11-20 09:45:47 -0300

    Modificado

--- TpPython/TpDiDomenico.py
+++ TpPython/TpDiDomenico.py
@@ -17,18 +17,37 @@ def randomize_positions(G):
     nodos, aristas = G
     pos = {}
     fuerzas = {}
-    for node in nodos:
-        pos[node] = (random.uniform(0, rangoX), random.uniform(0, rangoY))
-        fuerzas[node] = (0,0)
+    for nodo in nodos:
+        pos[nodo] = (random.uniform(0, rangoX), random.uniform(0, rangoY))
+        fuerzas[nodo] = (0,0)
     return nodos,aristas,pos,fuerzas
 
-def f_attraction(edge):
+def f_atraccion(grafo):
+    nodos,aristas,pos,fuerzas = grafo
+    for n1,n2 in aristas:
+        dist = math.sqrt((pos[n1][1]-pos[n2][1])**2 + (pos[n1][2]-pos[n2][2])**2)
+        
     pass
 
-def f_repulsion(n1, n2):
+def f_repulsion(grafo):
     pass
 
-def actualizo_posiciones():
+def reiniciar_acumuladores(grafo):
+    nodos,aristas,pos,fuerzas = grafo
+    for nodo in fuerzas:
+        fuerzas[nodo] = (0,0)
+    return nodos,aristas,pos,fuerzas
+    pass
+
+def actualizo_posiciones(grafo):
+    reiniciar_acumuladores(grafo)
+    nodos,aristas,pos,fuerzas = grafo
+    fa = f_atraccion(grafo)
+    fr = f_repulsion(grafo)
+    for n in nodos:
+        pos[n][1] = pos[n][1] + fuerzas[n][1]
+        pos[n][2] = pos[n][2] + fuerzas[n][2]
+    return nodos,aristas,pos,fuerzas
     pass
 
 
@@ -42,20 +61,21 @@ def layout(grafo):
         for n in nodos:
             x,y = pos[n]
             pygame.draw.circle(screen, RED, [int(round(x)),int(round(y))], 5)
-            
+            texto = fuente1.render(str(n), False, BLACK)
+            screen.blit(texto,[int(round(x)-5),int(round(y)-20)])
     pass
 
+pygame.init()
 screen = pygame.display.set_mode([rangoX,rangoY])
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
-BTree = ([1,2,3,4,5,6,7],[(4,2),(4,6),(2,1),(2,3),(6,5),(6,7)])
-fuente1 = ('Arial', 15)
+fuente1 = pygame.font.SysFont('Serif', 15)
 
 def main():
-    pygame.init()
+    BTree = ([1,2,3,4,5,6,7],[(4,2),(4,6),(2,1),(2,3),(6,5),(6,7)])
     n = 1
     BTree = randomize_positions(BTree)
 
