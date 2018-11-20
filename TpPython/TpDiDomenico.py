b9559c01aec162d1ad3237fea8cfd89ba342f328
FaustoDiDomenico <faustodido@gmail.com>
2018-11-20 06:40:26 -0300

    Agregado virtualEnv, y modificado la que dibuja un grafo.

--- TpPython/TpDiDomenico.py
+++ TpPython/TpDiDomenico.py
@@ -7,11 +7,11 @@
 # Para descargar py-gnuplot: http://sourceforge.net/projects/gnuplot-py/files/latest/download?source=files
 
 import time
-import Gnuplot
+import pygame
 import random
 
-rangoX = 100
-rangoY = 100
+rangoX = 800
+rangoY = 600
 
 def randomize_positions(G):
     nodos, aristas = G
@@ -38,14 +38,35 @@ def layout(grafo):
         for n1,n2 in aristas:
             x1,y1 = pos[n1]
             x2,y2 = pos[n2]
-            #dibujar las aristas
+            pygame.draw.line(screen, BLUE, [int(round(x1)),int(round(y1))], [int(round(x2)),int(round(y2))],2)
         for n in nodos:
-            x,y = n
-            #dibujar los nodos
+            x,y = pos[n]
+            pygame.draw.circle(screen, RED, [int(round(x)),int(round(y))], 5)
+            
     pass
 
+screen = pygame.display.set_mode([rangoX,rangoY])
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+BTree = ([1,2,3,4,5,6,7],[(4,2),(4,6),(2,1),(2,3),(6,5),(6,7)])
+fuente1 = ('Arial', 15)
 
 def main():
+    pygame.init()
+    n = 1
+    BTree = randomize_positions(BTree)
+
+    while n:
+        screen.fill(WHITE)
+        layout(BTree)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                n=False
+    pygame.quit()
     pass
 
 
